b370e667c0c1ada8bfb85dd3a9f42ba69620053c
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-04-15 19:21:31 +0200

    Bug: Asynchronous function

--- escapeVanillaJS.js
+++ escapeVanillaJS.js
@@ -11,15 +11,22 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     document.getElementById("solveRoom2").addEventListener("click", () => {
-        const jsConcepts = new Set(['closure', 'scope', 'hoisting']);
+        const jsConcepts = new Set(['closure', 'scope', 'hoisting',  'prototype']);
         // 🪲 Bug: What's mssing from JS concepts?
         const reactConcepts = new Set(['components', 'jsx', 'hooks', 'async']);
         // 🪲 Bug: Incorrect function call
-        const commonConcepts = findIntersection(jsConcepts, jsConcepts);
+        const commonConcepts = findIntersection(jsConcepts, reactConcepts);  // finding the intersection between JS and React concepts.
         document.getElementById("room2Result").textContent = `The code to unlock the door is: ${Array.from(commonConcepts).join(', ')}`;
     });
 
     // 🪲 Bug: Asynchronous function ?
+    async function navigateLabyrinth(directions) {
+        for (let direction of directions) {
+          await new Promise(resolve => setTimeout(resolve, 1000));
+          console.log(`Navigating: ${direction.step}`);
+        }
+        return "Congratulations! You've mastered the essentials of Vanilla JavaScript. Welcome to the world of React, where you'll build powerful and dynamic web applications. Let's dive in!";
+      }
     document.getElementById("solveRoom3").addEventListener("click", () => {
         fetch('directions.json') 
             .then(response => response.json())
@@ -27,7 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 navigateLabyrinth(directions)
                     .then(message => {
                         // 🪲 Bug: Incorrect method
-                        document.getElementById("room3Result").innerHTML = message;
+                        document.getElementById("room3Result").textContent = message;
                     });
             });
     });
